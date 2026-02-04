d7a16bd807a0f3c54414c52a8bb6b6bc6d1d860f
mrquaydelacruz-sys <mr.quaydelacruz@gmail.com>
2026-02-04 16:29:17 -0700

    Update env.ts

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,5 +1,6 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-12-16'
+// Force redeploy
 
 export const dataset = assertValue(
   (process.env.NEXT_PUBLIC_SANITY_DATASET || 'production').replace(/["']/g, "").trim(),
